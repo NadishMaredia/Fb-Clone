841f5874fdac59c08dad3223d19a90d2d00d1e09
Nadish Maredia <Nadish Maredia@NADISH>
2021-01-16 19:33:31 +0500

    Header Added

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCKaDTs4NCGacb6PEE4ehAFbLnAldQcLwQ",
+    authDomain: "fb-clone-c9bef.firebaseapp.com",
+    projectId: "fb-clone-c9bef",
+    storageBucket: "fb-clone-c9bef.appspot.com",
+    messagingSenderId: "265851720491",
+    appId: "1:265851720491:web:94f74c07607d8a54c62b6e"
+};
