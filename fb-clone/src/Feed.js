4f396fc23a4ae56c76796f62778ecd62f83417c6
Nadish Maredia <Nadish Maredia@NADISH>
2021-01-17 15:54:14 +0500

    Design Phase 3

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Feed.css';
 import StoryReed from './StoryReed';
+import MessageSender from './MessageSender';
 
 function Feed() {
     return (
@@ -8,6 +9,7 @@ function Feed() {
             {/* story */}
             <StoryReed />
             {/* MessageSender */}
+            <MessageSender />
         </div>
     )
 }
